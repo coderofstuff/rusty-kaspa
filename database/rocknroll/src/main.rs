8366be7b32b3f8733e301cc72aa190a13dad3dc0
Michael Sutton <msutton@cs.huji.ac.il>
2025-10-06 00:56:34 +0000

    count accepted txs in 24 hour range

--- database/rocknroll/src/main.rs
+++ database/rocknroll/src/main.rs
@@ -1,9 +1,17 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-use std::{mem::size_of, sync::Arc};
+use chrono::{TimeZone, Utc};
+use std::{
+    mem::size_of,
+    sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
-use kaspa_consensus::consensus::storage::ConsensusStorage;
+use kaspa_consensus::{
+    consensus::{services::ConsensusServices, storage::ConsensusStorage},
+    model::stores::{acceptance_data::AcceptanceDataStoreReader, headers::HeaderStoreReader, pruning::PruningStoreReader},
+};
 use kaspa_consensus_core::{
     config::ConfigBuilder,
     network::{NetworkId, NetworkType},
@@ -12,40 +20,41 @@ use kaspa_consensus_core::{
 use kaspa_core::info;
 use kaspad_lib::daemon::{get_app_dir, CONSENSUS_DB, DEFAULT_DATA_DIR, META_DB, UTXOINDEX_DB};
 
-fn to_human_readable(mut number_to_format: f64, precision: usize, suffix: &str) -> String {
-    const UNITS: [&str; 7] = ["", "K", "M", "G", "T", "P", "E"];
-    const DIV: [f64; 7] =
-        [1.0, 1_000.0, 1_000_000.0, 1_000_000_000.0, 1_000_000_000_000.0, 1_000_000_000_000_000.0, 1_000_000_000_000_000_000.0];
-    let i = (number_to_format.log(1000.0) as usize).min(UNITS.len() - 1);
-    number_to_format /= DIV[i];
-    format!("{number_to_format:.precision$}{}{}", UNITS[i], suffix)
-}
-
 fn main() {
     kaspa_core::log::init_logger(None, "");
-    let network = NetworkId::with_suffix(NetworkType::Testnet, 11);
+    let network = NetworkId::new(NetworkType::Mainnet);
     let app_dir = get_app_dir();
     let db_dir = app_dir.join(network.to_prefixed()).join(DEFAULT_DATA_DIR);
-    let consensus_db_dir = db_dir.join(CONSENSUS_DB).join("consensus-001");
-    // let utxoindex_db_dir = db_dir.join(UTXOINDEX_DB);
-    // let meta_db_dir = db_dir.join(META_DB);
+    let consensus_db_dir = db_dir.join(CONSENSUS_DB).join("consensus-003"); // check your own index
+                                                                            // let utxoindex_db_dir = db_dir.join(UTXOINDEX_DB);
+                                                                            // let meta_db_dir = db_dir.join(META_DB);
 
     let config = Arc::new(ConfigBuilder::new(network.into()).adjust_perf_params_to_consensus_params().build());
     let db =
         kaspa_database::prelude::ConnBuilder::default().with_db_path(consensus_db_dir).with_files_limit(128).build_readonly().unwrap();
 
-    let storage = ConsensusStorage::new(db, config);
+    let storage = ConsensusStorage::new(db.clone(), config.clone());
+    let services = ConsensusServices::new(db, storage.clone(), config, Default::default(), Default::default());
+
+    let start: SystemTime = Utc.with_ymd_and_hms(2025, 10, 5, 0, 0, 0).unwrap().into();
+    let end: SystemTime = Utc.with_ymd_and_hms(2025, 10, 6, 0, 0, 0).unwrap().into();
+
+    let pp = storage.pruning_point_store.read().pruning_point().unwrap();
+    let sink = storage.lkg_virtual_state.load().ghostdag_data.selected_parent;
+
+    let (start, end) =
+        (start.duration_since(UNIX_EPOCH).unwrap().as_millis() as u64, end.duration_since(UNIX_EPOCH).unwrap().as_millis() as u64);
     let mut count = 0;
-    let mut bytes = 0;
-    for (_, entry) in storage.pruning_utxoset_stores.read().utxo_set.iterator().map(|p| p.unwrap()) {
-        count += 1;
-        bytes += size_of::<TransactionOutpoint>();
-        bytes += size_of::<UtxoEntry>() - size_of::<ScriptVec>() + entry.script_public_key.script().len();
+
+    for cb in services.reachability_service.forward_chain_iterator(pp, sink, false) {
+        let timestamp = storage.headers_store.get_timestamp(cb).unwrap();
+        if start <= timestamp && timestamp < end {
+            let ad = storage.acceptance_data_store.get(cb).unwrap();
+            let mergeset_accepted_txs_count = ad.iter().map(|d| d.accepted_transactions.len()).sum::<usize>();
+            count += mergeset_accepted_txs_count;
+            if (count - mergeset_accepted_txs_count) / 1_000_000 != count / 1_000_000 {
+                info!("Accepted txs in range: {}", count);
+            }
+        }
     }
-    info!("UTXO set count: {}, size: {}", count, to_human_readable(bytes as f64, 3, "B"));
-
-    let full_blocks = storage.block_transactions_store.iterator().count();
-    dbg!(full_blocks);
-
-    // drop(db);
 }
